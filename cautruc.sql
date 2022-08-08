bf0260c0fd365bc8670d4e3f3d0d73e37b143fe2
KhggVuu <109039985+KhggVuu@users.noreply.github.com>
2022-08-08 08:29:37 +0700

    update data

--- cautruc.sql
+++ cautruc.sql
@@ -2,40 +2,72 @@ create database Asm191;
 
 use Asm191;
 
-create table categrory
+create table categorys
 (
-	categoryID varchar(5) not null primary key,
-    categoryName varchar(30) not null unique
+	categoryID nvarchar(5) not null primary key,
+    categoryName nvarchar(50) not null
 );
 
-create table product
+create table producers
 (
-	productID varchar(5) not null primary key,
+	producerID nvarchar(5) primary key,
+    producerName varchar(30) not null unique
+);
+
+create table products
+(
+	productID varchar(10) not null primary key,
     productName varchar(50) not null,
-    prince int not null,
-    image varchar(30) not null,
-	details varchar(300) not null,
-	categoryID varchar(3) not null,
-    
-	constraint fk_categoryID foreign key(categoryID)
-                        references category(categoryID)
+    productPrice float not null,
+    productDetails varchar(3000) null,
+    productImage1 varchar(30) not null,
+    productImage2 varchar(30) null,
+    productImage3 varchar(30) null,
+    producerID nvarchar(5) not null,
+    categoryID nvarchar(5) not null,
+
+    constraint foreign key (producerID) references producers(producerID),
+    constraint foreign key (categoryID) references categorys(categoryID)
 );
 
-create table customer
+create table admins
 (
-    username varchar(20) not null primary key,
-    password varchar(20) not null,
-    fullName varchar(30) not null,
-    address varchar(30) not null, 
-    email varchar(30) not null,
-    phone varchar(10) not null
-);
-create table admin
-(
-    adminID varchar(10) not null primary key,
-    adminPwd varchar(20) not null,
+	adminID nvarchar(10) primary key,
+    `password` nvarchar(30) not null,
     fullname nvarchar(30) not null,
-    email varchar(20) not null
+    address nvarchar(30) not null,
+    phone nvarchar(30) not null,
+    email nvarchar(30) not null
 );
 
+create table customers
+(
+	username nvarchar(30) not null primary key,
+    `password` nvarchar(30) not null,
+    fullname nvarchar(30) not null,
+    address nvarchar(30) not null,
+    phone nvarchar(30) not null,
+    email nvarchar(30) not null
+);
+
+create table invoiceDetails
+(
+	invoiceDetailID nvarchar(10) not null primary key,
+	`date` nvarchar(30) not null,
+    price float not null,
+    quantity int not null,
+    productID varchar(10) not null,
+
+    constraint foreign key (productID) references products(productID)
+);
+
+create table invoices
+(
+	invoiceID nvarchar(10) not null primary key,
+    invoiceDetailID nvarchar(10) not null,
+    username nvarchar(30) not null,
+
+    constraint foreign key (username) references customers(username),
+    constraint foreign key (invoiceDetailID) references invoiceDetails(invoiceDetailID)
+);
 
