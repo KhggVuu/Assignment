b1d754be17fcd667bcb10751cbe56791644d139b
KhggVuu <109039985+KhggVuu@users.noreply.github.com>
2022-08-15 11:39:01 +0700

    update next

--- resources/views/layouts/inc/admin/sidebar.blade.php
+++ resources/views/layouts/inc/admin/sidebar.blade.php
@@ -4,13 +4,15 @@
       <li class="nav-item">
         <a class="nav-link" data-toggle="collapse" href="#ui-basic" aria-expanded="false" aria-controls="ui-basic">
           <i class="icon-disc menu-icon"></i>
-          <span class="menu-title">UI Elements</span>
+          <span class="menu-title">Product Store</span>
           <i class="menu-arrow"></i>
         </a>
         <div class="collapse" id="ui-basic">
           <ul class="nav flex-column sub-menu">
-            <li class="nav-item"> <a class="nav-link" href="pages/ui-features/buttons.html">Buttons</a></li>
-            <li class="nav-item"> <a class="nav-link" href="pages/ui-features/typography.html">Typography</a></li>
+            <li class="nav-item"><a href="{{url('x')}}" class="nav-link">Product</a></li>
+            <li class="nav-item"><a href="{{url('x')}}" class="nav-link">Add new</a></li>
+            <li class="nav-item"><a href="{{url('x')}}" class="nav-link">Edit</a></li>
+            <li class="nav-item"><a href="{{url('x')}}" class="nav-link">Delete</a></li>
           </ul>
         </div>
       </li>
